896fb627d6810f47ee207f8bef3e7bce288a689c
Your Name <you@example.com>
2023-07-29 18:28:48 +0530

    push build

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -38,7 +38,7 @@ export default function Checkout() {
               <h3 className="font-semibold  text-gray-600 text-xs uppercase md:w-1/5 text-center md:block hidden">Price</h3>
               <h3 className="font-semibold  text-gray-600 text-xs uppercase w-1/5 text-center">Total</h3>
             </div>
-            {cart.cart.length === 0 ? <h1 className='text-red-600 text-center text-xl '>Your Cart is Empty</h1> : cart.cart.map((item) => {
+            {cart.cart.length === 0 ? <h1 className='text-red-600 text-center text-xl pb-10'>Your Cart is Empty</h1> : cart.cart.map((item) => {
               return (
                 <div className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5" key={item.idMeal}>
                   <div className="flex md:w-2/5 w-3/5">
@@ -67,7 +67,7 @@ export default function Checkout() {
                 </div>
               )
             })}
-            <Link to='/' className="flex font-semibold text-indigo-600 text-sm mt-10 absolute bottom-0 lg:bottom-5 ">
+            <Link to='/' className="flex font-semibold text-indigo-600 text-sm mt-10 absolute bottom-0  lg:bottom-5 ">
               <svg className="fill-current mr-2 text-indigo-600 w-4" viewBox="0 0 448 512"><path d="M134.059 296H436c6.627 0 12-5.373 12-12v-56c0-6.627-5.373-12-12-12H134.059v-46.059c0-21.382-25.851-32.09-40.971-16.971L7.029 239.029c-9.373 9.373-9.373 24.569 0 33.941l86.059 86.059c15.119 15.119 40.971 4.411 40.971-16.971V296z" /></svg>
               Add more Items
             </Link>
